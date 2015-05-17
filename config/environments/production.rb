10ab4d2d723904e0d80325f750602be4b6983666
laomatt <laomatt1@gmail.com>
2015-05-17 16:40:13 -0700

    changed production environment to try and connect to mongo lab

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,29 +3,25 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  seed_data = [
-    {
-      'name' => '1970s',
-      'city' => 'Debby Boone',
-      'state' => 'You Light Up My Life',
-      'show' => '10'
-      },
-      {
-        'name' => '1980s',
-        'city' => 'Olivia Newton-John',
-        'state' => 'Physical',
-        'show' => '10'
-        }]
+  # seed_data = [
+  #   {
+  #     'name' => '1970s',
+  #     'city' => 'Debby Boone',
+  #     'state' => 'You Light Up My Life',
+  #     'show' => '10'
+  #     },
+  #     {
+  #       'name' => '1980s',
+  #       'city' => 'Olivia Newton-John',
+  #       'state' => 'Physical',
+  #       'show' => '10'
+  #       }]
 
         uri="mongodb://matt_lao:trouble@ds031822.mongolab.com:31822/characters"
+        client= Mongo::Client.new(uri)
+        # db=client.db("characters")
+        # chars = db.collections("characters")
 
-        client= Mongo::MongoClient.from_uri(uri)
-
-        db=client.db("characters")
-
-        chars = db.collections("characters")
-
-        chars.insert(seed_data)
 
   # Code is not reloaded between requests.
   config.cache_classes = true
