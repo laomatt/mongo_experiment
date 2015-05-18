f5e578531a52ca46e06591d2d0009541ea4b7ad9
laomatt <laomatt1@gmail.com>
2015-05-18 16:58:33 -0700

    commented out line 2 in person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,9 @@
 class Person
   # include Mongoid::Document
-  include Mongo::Document
-  field :name, type: String
-  field :show, type: String
-  field :pic, type: String
-  field :city, type: String
-  field :state, type: String
+  # include Mongo::Document
+  # field :name, type: String
+  # field :show, type: String
+  # field :pic, type: String
+  # field :city, type: String
+  # field :state, type: String
 end
